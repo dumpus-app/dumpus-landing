27fb83b3a4944d5088b4b6ae503447564cc20cc5
Mittelblut9 <info@blackdayz.de>
2023-08-09 23:43:48 +0200

    fix: add router-link tag to prevent page reloads

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -24,13 +24,13 @@
             </a>
         </div>
         <div class="footerLinks">
-            <a href="/" target="_self">Home</a>
+            <router-link to="/" target="_self">Home</router-link>
             <a href="https://github.com/dumpus-app/dumpus-app/releases" target="_blank"
                 >What's New</a
             >
-            <a href="/legal/privacy" target="_self">Privacy Policy</a>
-            <a href="/legal/terms" target="_self">Terms & Conditions</a>
-            <a href="/help" target="_self">Help</a>
+            <router-link to="/legal/privacy" target="_self">Privacy Policy</router-link>
+            <router-link to="/legal/terms" target="_self">Terms & Conditions</router-link>
+            <router-link to="/help" target="_self">Help</router-link>
         </div>
     </footer>
 </template>
